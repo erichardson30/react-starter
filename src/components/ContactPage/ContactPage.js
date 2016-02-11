f702017b57d19949d38da488553df976fff03ebe
Eric Richardson <eric.richardson30@gmail.com>
2016-02-11 15:15:53 -0500

    updated documentation

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,3 +1,12 @@
+/**
+ * React Starter (https://github.com/erichardson30/react-starter)
+ *
+ * Copyright © 2016 Eric Richardson. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+ 
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './ContactPage.scss';
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,3 +1,12 @@
+/**
+ * React Starter (https://github.com/erichardson30/react-starter)
+ *
+ * Copyright © 2016 Eric Richardson. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
 import alt from '../core/alt';
 import AppActions from '../actions/AppActions';
 
