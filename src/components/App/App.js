fa081c498a32b6d7ea135c98c7e99cf513dabfc0
Richardson, Eric <erichardson@CSGRALLT54.cardinalsolutions.com>
2016-02-12 13:34:50 -0500

    Implemented React-Router instead of the react-routing this came with

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,60 +7,40 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { Component, PropTypes } from 'react';
-import emptyFunction from 'fbjs/lib/emptyFunction';
-import s from './App.scss';
-import Header from '../Header';
-import Footer from '../Footer';
+ import React, { Component, PropTypes } from 'react';
+ import s from './App.scss';
+ import Header from '../Header';
+ import Footer from '../Footer';
 
-class App extends Component {
+ class App extends Component {
 
-  static propTypes = {
-    context: PropTypes.shape({
-      insertCss: PropTypes.func,
-      onSetTitle: PropTypes.func,
-      onSetMeta: PropTypes.func,
-      onPageNotFound: PropTypes.func,
-    }),
-    children: PropTypes.element.isRequired,
-    error: PropTypes.object,
-  };
+   static propTypes = {
+     children: PropTypes.element.isRequired,
+     error: PropTypes.object,
+   };
 
-  static childContextTypes = {
-    insertCss: PropTypes.func.isRequired,
-    onSetTitle: PropTypes.func.isRequired,
-    onSetMeta: PropTypes.func.isRequired,
-    onPageNotFound: PropTypes.func.isRequired,
-  };
+   static contextTypes = {
+     insertCss: PropTypes.func,
+   };
 
-  getChildContext() {
-    const context = this.props.context;
-    return {
-      insertCss: context.insertCss || emptyFunction,
-      onSetTitle: context.onSetTitle || emptyFunction,
-      onSetMeta: context.onSetMeta || emptyFunction,
-      onPageNotFound: context.onPageNotFound || emptyFunction,
-    };
-  }
+   componentWillMount() {
+     this.removeCss = this.context.insertCss(s);
+   }
 
-  componentWillMount() {
-    this.removeCss = this.props.context.insertCss(s);
-  }
+   componentWillUnmount() {
+     this.removeCss();
+   }
 
-  componentWillUnmount() {
-    this.removeCss();
-  }
+   render() {
+     return !this.props.error ? (
+       <div>
+         <Header />
+         {this.props.children}
+         <Footer />
+       </div>
+     ) : this.props.children;
+   }
 
-  render() {
-    return !this.props.error ? (
-      <div>
-        <Header />
-        {this.props.children}
-        <Footer />
-      </div>
-    ) : this.props.children;
-  }
+ }
 
-}
-
-export default App;
+ export default App;
--- src/server.js
+++ src/server.js
@@ -7,59 +7,71 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import 'babel-polyfill';
-import path from 'path';
-import express from 'express';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import Router from './routes';
-import Html from './components/Html';
-import assets from './assets';
-import { port } from './config';
+ import 'babel-polyfill';
+ import path from 'path';
+ import express from 'express';
+ import React from 'react';
+ import ReactDOM from 'react-dom/server';
+ import { match, RouterContext } from 'react-router';
+ import assets from './assets';
+ import { port } from './config';
+ import routes from './routes';
+ import ContextHolder from './core/ContextHolder';
+ import Html from './components/Html';
 
-const server = global.server = express();
+ const server = global.server = express();
 
-//
-// Register Node.js middleware
-// -----------------------------------------------------------------------------
-server.use(express.static(path.join(__dirname, 'public')));
+ //
+ // Register Node.js middleware
+ // -----------------------------------------------------------------------------
+ server.use(express.static(path.join(__dirname, 'public')));
 
-//
-// Register API middleware
-// -----------------------------------------------------------------------------
-server.use('/api/content', require('./api/content').default);
+ //
+ // Register API middleware
+ // -----------------------------------------------------------------------------
+ server.use('/api/content', require('./api/content').default);
 
-//
-// Register server-side rendering middleware
-// -----------------------------------------------------------------------------
-server.get('*', async (req, res, next) => {
-  try {
-    let statusCode = 200;
-    const data = { title: '', description: '', css: '', body: '', entry: assets.main.js };
-    const css = [];
-    const context = {
-      insertCss: styles => css.push(styles._getCss()),
-      onSetTitle: value => data.title = value,
-      onSetMeta: (key, value) => data[key] = value,
-      onPageNotFound: () => statusCode = 404,
-    };
+ //
+ // Register server-side rendering middleware
+ // -----------------------------------------------------------------------------
+ server.get('*', async (req, res, next) => {
+   try {
+     match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+       if (error) {
+         throw error;
+       }
+       if (redirectLocation) {
+         const redirectPath = `${ redirectLocation.pathname }${ redirectLocation.search }`;
+         res.redirect(302, redirectPath);
+         return;
+       }
+       let statusCode = 200;
+       const data = { title: '', description: '', css: '', body: '', entry: assets.main.js };
+       const css = [];
+       const context = {
+         insertCss: styles => css.push(styles._getCss()),
+         onSetTitle: value => data.title = value,
+         onSetMeta: (key, value) => data[key] = value,
+         onPageNotFound: () => statusCode = 404,
+       };
+       data.body = ReactDOM.renderToString(
+         <ContextHolder context={context}>
+           <RouterContext {...renderProps}/>
+         </ContextHolder>
+       );
+       data.css = css.join('');
+       const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
+       res.status(statusCode).send(`<!doctype html>\n${html}`);
+     });
+   } catch (err) {
+     next(err);
+   }
+ });
 
-    await Router.dispatch({ path: req.path, query: req.query, context }, (state, component) => {
-      data.body = ReactDOM.renderToString(component);
-      data.css = css.join('');
-    });
-
-    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
-    res.status(statusCode).send(`<!doctype html>\n${html}`);
-  } catch (err) {
-    next(err);
-  }
-});
-
-//
-// Launch the server
-// -----------------------------------------------------------------------------
-server.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`The server is running at http://localhost:${port}/`);
-});
+ //
+ // Launch the server
+ // -----------------------------------------------------------------------------
+ server.listen(port, () => {
+   /* eslint-disable no-console */
+   console.log(`The server is running at http://localhost:${port}/`);
+ });
